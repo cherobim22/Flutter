b07b07590e684b630fcfde17e4bfb88187926aec
cherobim22 <lucascherobim@hotmail.com>
2022-05-15 00:41:48 -0300

    adding provider

--- basicapp/lib/main.dart
+++ basicapp/lib/main.dart
@@ -1,32 +1,39 @@
+import 'package:basicapp/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
-import 'screens/categories_screen.dart';
+import 'models/auth.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
+import 'screens/tabs_screen.dart';
+import 'screens/auth_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        // '/': (context) => CategoriesScreen(),
-        '/categories-meals': (context) => CategoresMealsScreen(),
-        '/meal-details': (context) => MealDeatilScreen()
-      },
-      title: 'Vamos Cozinhar?',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => Auth())],
+      child: MaterialApp(
+        title: 'Vamos Cozinhar?',
+        theme: ThemeData(
+          primarySwatch: Colors.lightBlue,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                ),
               ),
-            ),
+        ),
+        routes: {
+          '/': (context) => AuthScreen(),
+          '/home': (context) => TabsScreen(),
+          '/categories-meals': (context) => CategoresMealsScreen(),
+          '/meal-details': (context) => MealDeatilScreen()
+        },
       ),
-      home: CategoriesScreen(),
     );
   }
 }
