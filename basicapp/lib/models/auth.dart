18e3a640b86fca4cb16a242a6882effc518ec56a
cherobim22 <lucascherobim@hotmail.com>
2022-05-17 23:09:38 -0300

    clean

--- basicapp/lib/models/auth.dart
+++ basicapp/lib/models/auth.dart
@@ -4,8 +4,6 @@ import 'package:basicapp/exceptions/auth_exception.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-// https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBV83FL3LjD9bwn625PFZq8-RKoRwQzVRQ
-
 class Auth with ChangeNotifier {
   String? _token;
   String? _email;
