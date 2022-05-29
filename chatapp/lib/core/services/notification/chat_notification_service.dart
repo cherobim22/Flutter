dd5df70e696bcad253d7789b30f0dc179fd73d4d
cherobim22 <lucascherobim@hotmail.com>
2022-05-29 12:55:28 -0300

    adding local notifications

---
+++ chatapp/lib/core/services/notification/chat_notification_service.dart
@@ -0,0 +1,19 @@
+import 'package:chatapp/core/models/chat_notification.dart';
+import 'package:flutter/material.dart';
+
+class ChatNotificationService with ChangeNotifier {
+  List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+  }
+}
