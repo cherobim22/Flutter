33993dba43a154003bcb4d7308ddfce085cfc527
cherobim22 <lucascherobim@hotmail.com>
2022-05-29 12:35:47 -0300

    adding new messages

--- chatapp/lib/components/new_message.dart
+++ chatapp/lib/components/new_message.dart
@@ -1,12 +1,49 @@
+import 'package:chatapp/core/services/auth/auth_service.dart';
+import 'package:chatapp/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatelessWidget {
+class NewMessage extends StatefulWidget {
   const NewMessage({Key? key}) : super(key: key);
 
   @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _newMessage = '';
+  final _messageController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+    if (user != null) {
+      ChatService().save(_newMessage, user);
+      _messageController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Nova Mensagem'),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  _newMessage = value;
+                });
+              },
+              controller: _messageController,
+              decoration: InputDecoration(labelText: 'Enviar Mensagem'),
+            ),
+          ),
+          IconButton(
+            onPressed: _newMessage.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(Icons.send),
+          )
+        ],
+      ),
     );
   }
 }
