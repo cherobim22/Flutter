7b4814d66844ee7a17823a1f8e34c0e96538cef8
cherobim22 <lucascherobim@hotmail.com>
2022-05-30 21:05:59 -0300

    adding push notification

--- chatapp/lib/pages/chat_page.dart
+++ chatapp/lib/pages/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:chatapp/components/messages.dart';
 import 'package:chatapp/components/new_message.dart';
 import 'package:chatapp/core/services/auth/auth_service.dart';
+import 'package:chatapp/pages/notification_page.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -12,34 +13,61 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat App'),
         actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.black87,
+                        ),
+                        SizedBox(width: 10),
+                        Text('Sair'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
+              },
             ),
-            items: [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Container(
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Colors.black87,
-                      ),
-                      SizedBox(width: 10),
-                      Text('Sair'),
-                    ],
+          ),
+          Stack(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        return NotificationPage();
+                      }),
+                    );
+                  },
+                  icon: Icon(Icons.notifications)),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    '0',
+                    style: TextStyle(fontSize: 12),
                   ),
                 ),
               ),
             ],
-            onChanged: (value) {
-              if (value == 'logout') {
-                AuthService().logout();
-              }
-            },
-          ),
+          )
         ],
       ),
       body: SafeArea(
