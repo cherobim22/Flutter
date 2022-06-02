1118c0c6db7d0d16f19e7d8297035e80cd3fd7f9
cherobim22 <lucascherobim@hotmail.com>
2022-06-01 22:31:48 -0300

    update files

--- chatapp/lib/pages/notification_page.dart
+++ chatapp/lib/pages/notification_page.dart
@@ -1,17 +1,28 @@
+import 'package:chatapp/core/services/notification/chat_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final service = Provider.of<ChatNotificationService>(context);
+    final items = service.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minhas notificações'),
       ),
-      body: Center(child: Text('Notificações')),
+      body: ListView.builder(
+        itemCount: service.itemsCount,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(items[index].title),
+          subtitle: Text(items[index].body),
+          onTap: () => service.remove(index),
+        ),
+      ),
     );
   }
 }
